5099c51d88f61266a647a77933247e82b217e81d
IzzetMustafaoglu <izzetmustafaoglu00@gmail.com>
2023-08-25 09:03:48 +0300

    Icon Ekleme

--- lib/main.dart
+++ lib/main.dart
@@ -6,6 +6,7 @@ void main() {
 class benimUyg extends StatelessWidget{
   Widget build(BuildContext context){
     return MaterialApp(
+    //  theme: ThemeData(fontFamily: 'Satisfy'),  // Tüm yazıları Satisfy fontu olarak ayarlar
       home: Scaffold(
         backgroundColor: Colors.brown[300],
         body: SafeArea(
@@ -26,6 +27,61 @@ class benimUyg extends StatelessWidget{
                     color: Colors.brown[900],
                   ),
                 ),
+                Text(
+              "Bir Fincan Uzağında",
+              style: TextStyle(
+                fontSize: 16,
+                color: Colors.white,
+
+              ),
+            ),
+                SizedBox(
+                  height: 10,
+                ),
+                Container(
+                  margin: EdgeInsets.symmetric(horizontal: 45.0), // iki taraftan boşluk
+                  padding: EdgeInsets.all(10.0),
+                  color: Colors.brown,
+                  child: Row(
+                    children: <Widget>[
+                      Icon(
+                        Icons.email,
+                        color: Colors.white,
+                      ),
+                      SizedBox(width: 10.0,),
+                      Text(
+                        "sipariş@kahveci.com",
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 20.0,
+                        ),
+                      )
+                    ],
+                  ),
+                ),
+                SizedBox(
+                  height: 10,
+                ),
+                Container(
+                  margin: EdgeInsets.symmetric(horizontal: 45),
+                  padding: EdgeInsets.all(10),  // Hepsini büyütme
+                  color: Colors.brown,
+                  child: Row(
+                    children: <Widget>[
+                      Icon(
+                        Icons.phone,
+                        color: Colors.white,
+                      ),
+                      Text(
+                        "0544 482 32 65",
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 20.0,
+                        ),
+                      ),
+                    ],
+                  ),
+                )
               ],
             ),
           ),
